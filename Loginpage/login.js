6c2430afaef0d7fe8f0281663227cbc7df4ffd2a
lalitUnstopable <lalitpawar229@gmail.com>
2022-05-05 11:48:32 +0530

    loginpage

---
+++ Loginpage/login.js
@@ -0,0 +1,10 @@
+ 
+  document.querySelector("form").addEventListener("submit",loginpage)
+  function loginpage(){
+      var pass=document.querySelector("#pass").value 
+      var email=document.querySelector("#email").value 
+      var data=JSON.parse(localStorage.getItem("signupids"))
+     
+
+      
+  }
